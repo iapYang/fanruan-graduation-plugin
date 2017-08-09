e54d898742a188a88c9363cffe38519864478c69
DESKTOP-8IQMI96\Creasy <yklorse@gmail.com>
2017-08-09 14:14:29 +0800

    finish

--- dev/script/graduation/index.js
+++ dev/script/graduation/index.js
@@ -12,22 +12,36 @@ export default class {
         this.ctx = this.canvas.getContext('2d');
 
         this.angleA = 5 / 3;
+        this.angleTotal = 5 - 2 * this.angleA;
         this.largeR = 150;
         this.smallR = 130;
+        this.percentage = 0.6;
+
+        this.angleCalc = this.angleTotal * this.percentage;
 
         this.pointO = new Point(
             this.canvas.width / 2,
             this.canvas.height / 2
         );
 
+        this.pointE = new Point(
+            this.calcPointX(this.angleCalc, this.largeR),
+            this.calcPointY(this.angleCalc, this.largeR),
+        );
+
+        this.pointF = new Point(
+            this.calcPointX(this.angleCalc, this.smallR),
+            this.calcPointY(this.angleCalc, this.smallR),
+        );
+
         this.pointA = new Point(
-            this.pointO.getX() - this.largeR * Math.cos(this.angleA * Math.PI),
-            this.pointO.getY() - this.largeR * Math.sin(this.angleA * Math.PI)
+            this.calcPointX(this.angleA, this.largeR),
+            this.calcPointY(this.angleA, this.largeR)
         );
 
         this.pointB = new Point(
-            this.pointO.getX() - this.smallR * Math.cos(this.angleA * Math.PI),
-            this.pointO.getY() - this.smallR * Math.sin(this.angleA * Math.PI)
+            this.calcPointX(this.angleA, this.smallR),
+            this.calcPointY(this.angleA, this.smallR)
         );
 
         this.pointC = new Point(
@@ -42,41 +56,63 @@ export default class {
 
         this.$container.append(this.$canvas);
 
-        this.pointA.print();
-        this.pointB.print();
-        this.pointC.print();
-        this.pointD.print();
-
-
-        this.drawBorder();
+        this.draw(
+            this.pointA,
+            this.pointB,
+            this.pointC,
+            this.pointD,
+            this.angleA
+        );
     }
     drawBorder() {
-        const radius = (this.largeR - this.smallR) / 2;
-
         const pointAB = this.pointA.calcMiddlePoint(this.pointB);
         const pointCD = this.pointC.calcMiddlePoint(this.pointD);
+        const pointEF = this.pointE.calcMiddlePoint(this.pointF);
 
+        const radius = (this.largeR - this.smallR) / 2;
         const start = this.angleA - 1;
         const end = 2 - this.angleA;
 
         this.drawArc(pointAB, radius, start - 1, start);
         this.drawArc(pointCD, radius, 1 - start, -start);
+        this.drawArc(pointEF, radius, 1 - this.angleCalc, 2 - this.angleCalc);
 
-        this.drawArc(this.pointO, this.largeR, start, end);
-        this.drawArc(this.pointO, this.smallR, start, end);
+        // this.drawArc(this.pointO, this.largeR, start, end);
+        // this.drawArc(this.pointO, this.smallR, start, end);
     }
-    drawArc(point, radius, start, end) {
+    draw(pointA, pointB, pointC, pointD, angle, percentage = 1) {
+        const pointAB = pointA.calcMiddlePoint(pointB);
+        const pointCD = pointC.calcMiddlePoint(pointD);
+
+        const radius = (this.largeR - this.smallR) / 2;
+        const start = angle - 1;
+
+        this.ctx.beginPath();
+        
+        this.drawArc(pointAB, radius, start, start - 1, true);
+        this.drawArc(this.pointO, this.smallR, start, 1 - start);
+        this.drawArc(pointCD, radius, 1 - angle, 2 - angle, true);
+        this.drawArc(this.pointO, this.largeR, 2 - angle, angle - 1, true);
+    }
+    drawArc(point, radius, start, end, counterclockwise = false) {
         const startAngle = start * Math.PI;
         const endAngle = end * Math.PI;
         
-        this.ctx.beginPath();
         this.ctx.arc(
             point.getX(),
             point.getY(),
             radius,
             startAngle,
-            endAngle
+            endAngle,
+            counterclockwise
         );
         this.ctx.stroke();
+    }
+    calcPointX(angle, radius) {
+        return this.pointO.getX() -
+            radius * Math.cos(angle * Math.PI);
+    }
+    calcPointY(angle, radius) {
+        return this.pointO.getY() - radius * Math.sin(angle * Math.PI);
     }
 }
